0c738012bd997b752195bcdfc2e45c11e230b6ac
MazenWael2004 <115092034+MazenWael2004@users.noreply.github.com>
2024-11-04 18:26:59 +0200

    Added the Semaphore as an attribute

--- carThread.java
+++ carThread.java
@@ -1,11 +1,12 @@
 public class carThread extends Thread {
+    private EnterParkingSpotSemaphore enteringParkingSpot;
     private  int id;
     private double  arrival_time;
     private double parking_duration;// to define parking duration for each car 
     private String gate_name; // define gate name 1 or  2 or 3
 
-    public carThread( int id,double arrival_time,double parking_duration,String gate_name) {
-        this.id = id;
+    public carThread( EnterParkingSpotSemaphore p, int id,double arrival_time,double parking_duration,String gate_name) {
+        this.enteringParkingSpot = p;
         this.arrival_time = arrival_time;
         this.id = id;
         this.parking_duration= parking_duration;
