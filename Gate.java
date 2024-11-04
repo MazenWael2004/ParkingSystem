868fbc3fbef9cee9c2c6e4c632c6eb99d87c77d0
Roqaia Hassan <roqaiahassanahmed@gmail.com>
2024-11-04 21:43:44 +0200

    add constructor to gate

--- Gate.java
+++ Gate.java
@@ -1,6 +1,10 @@
 public class Gate {
     private int gate_id;
 
+    public Gate(int gate_id){
+        this.gate_id=gate_id;
+    }
+
     public void handleArrivalOfCar(carThread c) {
         c.start();
         
